90202055a074f9f9aa5ee1779d6e7886b7016312
SpencerOfwiti <maxspencer56@gmail.com>
2020-03-19 15:05:31 +0300

    Setup the signup authorization function. - Accept data typed into form by user. - Ensure user doesn't already exist. - Hash the password. - Add user to the database.

--- auth.py
+++ auth.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
-from __init__ import db
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from .__init__ import db
 
 auth = Blueprint('auth', __name__)
 
@@ -12,6 +14,31 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    # check if user with similar email exists in database
+    user = User.query.filter_by(email=email).first()
+
+    # if user is found, redirect back to signup page for user to try again
+    if user:
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    # create a new user with the form data
+    # hash the password so plaintext version isn't saved
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/logout')
