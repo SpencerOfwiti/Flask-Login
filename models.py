f55db23f1a7087e9b24f11661c163dc3e16aaa56
SpencerOfwiti <maxspencer56@gmail.com>
2020-03-19 12:11:04 +0300

    Create the User model. - Add fields for email address, password and name.

---
+++ models.py
@@ -0,0 +1,11 @@
+from . import db
+
+
+class User(db.Model):
+    """
+    Creating a User object
+    """
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
