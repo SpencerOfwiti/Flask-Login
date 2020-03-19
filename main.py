4c21e5dacac9e91ce8acfc6d8de07356c8b64dd8
SpencerOfwiti <maxspencer56@gmail.com>
2020-03-19 10:22:13 +0300

    Update the return statements in each of the routes.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from . import db
 
 main = Blueprint('main', __name__)
@@ -6,9 +6,9 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
 
 @main.route('/profile')
 def profile():
-    return 'Profile'
+    return render_template('profile.html')
