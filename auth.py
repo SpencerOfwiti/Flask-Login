aca17792a9f1dd3828b61510b5a7f01492412fab
SpencerOfwiti <maxspencer56@gmail.com>
2020-03-19 16:38:32 +0300

    Add the login method. - Check if email address is in database. - Hash the password provided by the user and compare it to hashed password in the database. - Log the user in using Flask-Login.

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
 from .models import User
 from .__init__ import db
 
@@ -9,6 +10,25 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if user exists
+    # take the user supplied password, hash it, and compare it to the hashed password in database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))  # reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
 
 
 @auth.route('/signup')
